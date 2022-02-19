12e208de67920090e4005749d99d5d9241310f6a
julialen <853917853917Qq>
2022-02-20 01:48:17 +0300

    add style for header, add swiper and some style for first screen

--- src/scripts/plugins/swiper.js
+++ src/scripts/plugins/swiper.js
@@ -1,10 +1,48 @@
-import Swiper, {Pagination, Navigation, Mousewheel, Keyboard} from 'swiper'
+import Swiper, {Pagination, Navigation, Mousewheel, Keyboard, EffectCreative} from 'swiper'
 
-Swiper.use([Pagination, Navigation, Mousewheel, Keyboard])
+Swiper.use([Pagination, Navigation, Mousewheel, Keyboard, EffectCreative])
 
 window.addEventListener('load', () => {
-  if (!document.querySelector('.swiper-container')) {
+  if (!document.querySelector('.firstscreen')) {
     return false
   }
+
+  const firstscreen = new Swiper(".firstscreen", {
+    speed: 600,
+    effect: 'creative',
+    creativeEffect: {
+      prev: {
+        // will set `translateZ(-400px)` on previous slides
+        translate: [0, 0, -400],
+      },
+      next: {
+        // will set `translateX(100%)` on next slides
+        translate: ['100%', 0, 0],
+      },
+    },
+    resizeObserver: true,
+    observer: true,
+    preloadImages: true,
+    lazy: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    direction: "horizontal",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: {
+      releaseOnEdges: true,
+      sensitivity: 2,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    keyboard: true,
+  })
+
 
 })
